71f99b3bf69b1df189507d25f045933519730616
Jarod Zhou <jarod@teemlink.com>
2017-05-02 20:56:21 +0800

    connect..

--- react3/app/index.jsx
+++ react3/app/index.jsx
@@ -5,14 +5,30 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import counter from '../reducers';
 import Connect1 from './Connect1';
+import Connect2 from './Connect2';
+import Connect3 from './Connect3';
 
 const store = createStore(counter, applyMiddleware(thunk));
 const rootEl = document.createElement('div');
 document.body.appendChild(rootEl);
 
 ReactDOM.render(
+  <div>
   <Provider store={store}>
-    <Connect1 />
-  </Provider>,
+    <div>
+        <Connect1 />
+    </div>
+  </Provider>
+  <Provider store={store}>
+    <div>
+        <Connect2 />
+    </div>
+  </Provider>
+  <Provider store={store}>
+    <div>
+        <Connect3 />
+    </div>
+  </Provider>
+  </div>,
   rootEl
 );
